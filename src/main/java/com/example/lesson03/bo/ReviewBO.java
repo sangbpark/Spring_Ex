6b13b335a4667c978ccadf5e2e8092bd256b4f02
sangbpark <qkrtkdqo94@gmail.com>
2024-09-30 21:46:53 +0900

    select 연습 20240930

---
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -0,0 +1,18 @@
+package com.example.lesson03.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson03.domain.Review;
+import com.example.lesson03.mapper.ReviewMapper;
+
+@Service
+public class ReviewBO {
+	
+	@Autowired
+	private ReviewMapper reviewMapper;
+	
+	public Review getReviewById(int id) {
+		return reviewMapper.selectReviewById(id);
+	}
+}
