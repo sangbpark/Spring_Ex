6615d3c7be69861dab6092b2f8a9d8a237d3d002
sangbpark <qkrtkdqo94@gmail.com>
2024-10-10 21:44:35 +0900

    mvc 20241010

--- src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex01Controller.java
@@ -2,12 +2,15 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.lesson04.bo.UserBO;
+import com.example.lesson04.domain.User;
 
 @RequestMapping("/lesson04/ex01")
 @Controller // HTML 경로일 때는 @ResponseBody가 없다
@@ -35,5 +38,20 @@ public class Lesson04Ex01Controller {
 		userBO.addUser(name, yyyymmdd, email, introduce);
 		// 결과 화면 이동
 		return "lesson04/afterAddUser";
+	}
+	
+	// 최신 가입자 정보
+	// http://localhost/lesson04/ex01/latest-user-view
+	@GetMapping("/latest-user-view")
+	public String latestUserView(Model model) {
+		// DB select
+		User user = userBO.getLatestUser();
+		
+		// Controller가 Model에 데이터를 집어넣는다.
+		// HTML(View)가 Model에서 꺼내서 쓴다.
+		model.addAttribute("result", user);
+		model.addAttribute("title", "최근 가입된 유저");
+		
+		return "lesson04/latestUser";
 	}
 }
