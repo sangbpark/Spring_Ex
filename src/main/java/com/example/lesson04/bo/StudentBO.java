85c217afe76e5ad21330205b5c92cb3dddfe9760
sangbpark <qkrtkdqo94@gmail.com>
2024-10-23 21:44:58 +0900

    jpa 시작 20241023

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -5,12 +5,30 @@ import org.springframework.stereotype.Service;
 
 import com.example.lesson04.domain.Student;
 import com.example.lesson04.mapper.StudentMapper;
+import com.example.lesson07.entity.StudentEntity;
+import com.example.lesson07.repository.StudentRepository;
 
 @Service
 public class StudentBO {
 	@Autowired
 	private StudentMapper studentMapper;
 	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	// JPA
+	public StudentEntity addStudent(String name, String phoneNumber
+			, String email, String dreamJob) {
+		StudentEntity student = StudentEntity.builder()
+				.name(name)
+				.phoneNumber(phoneNumber)
+				.email(email)
+				.dreamJob(dreamJob)
+				.build();
+		return studentRepository.save(student);
+	};
+	
+	// mybatis
 	public void addStudent(Student student) {
 		studentMapper.insertStudent(student);
 	};
---
+++ src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
@@ -0,0 +1,35 @@
+package com.example.lesson07;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson04.bo.StudentBO;
+import com.example.lesson07.entity.StudentEntity;
+
+@RequestMapping("/lesson07/ex01")
+@RestController
+public class Lesson07Ex01RestController {
+	@Autowired
+	private StudentBO studentBO;
+	
+	// C: create
+	@GetMapping("/create")
+	public StudentEntity create() {
+		String name = "박상배";
+		String phoneNumber = "01000000000";
+		String email = "skdfjld@naver.com";
+		String dreamJob = "셔터맨";
+		
+		// save가 된 객체를 리턴하므로 id가 채워져있다.
+		return studentBO.addStudent(name, phoneNumber, email, dreamJob);	
+	}
+	
+	// U: update
+	@GetMapping("/update")
+	public StudentEntity update() {
+		// id가 4인 dreamJob을 디자이너로 변경
+		return studentBO.updateStudentDreamJobById(4, "디자이너");
+	}
+}
